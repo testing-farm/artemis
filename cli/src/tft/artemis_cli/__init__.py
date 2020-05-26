87b8ca4479d8ed8dfd27b8b825d4925296e53134
Ondrej Ptak <optak@redhat.com>
2020-05-26 13:16:46 +0200

    artemis-cli: draft for discussion, minimal API implementation

---
+++ cli/src/tft/artemis_cli/__init__.py
@@ -0,0 +1,345 @@
+import concurrent.futures
+import json
+import pkg_resources
+import shlex
+import subprocess
+import sys
+
+import click
+import click_spinner
+import dataclasses
+import jsonschema
+import requests
+import ruamel.yaml
+import ruamel.yaml.compat
+import tabulate
+import urlnormalizer
+
+from typing import cast, Any, Callable, Dict, Iterable, List, NamedTuple, NoReturn, Optional, Tuple, TypeVar
+
+class ValidationResult(NamedTuple):
+    """
+    Represents schema validation result
+    """
+    result: bool
+    errors: List[Any]
+
+
+class Logger:
+    """
+    Simple class providing semantic logging.
+    """
+
+    def __init__(self, context: Optional[str] = None) -> None:
+        self.context = context
+
+        self._context_prefix = '[{}] '.format(context) if context else ''
+
+    def debug(self, msg: str) -> None:
+        # We need to introduce options controling logging output first!
+        # click.echo()
+        pass
+
+    def info(self, msg: str) -> None:
+        click.echo(YELLOW('{}{}'.format(self._context_prefix, msg)))
+
+    def warn(self, msg: str) -> None:
+        click.echo(YELLOW('{}{}'.format(self._context_prefix, msg)))
+
+    def error(self, msg: str) -> NoReturn:
+        click.echo(RED('{}{}'.format(self._context_prefix, msg)), err=True)
+
+        sys.exit(1)
+
+    def success(self, msg: str) -> None:
+        click.echo(GREEN('{}{}'.format(self._context_prefix, msg)))
+
+
+@dataclasses.dataclass
+class Configuration:
+    raw_config: Optional[Any] = None
+
+    logger: Optional[Logger] = None
+
+    config_dirpath: Optional[str] = None
+    config_filepath: Optional[str] = None
+
+    completion_shell: Optional[str] = None
+
+    output_format: str = 'human'
+
+    artemis_api_url: str = None
+
+# Colorization
+def BLUE(s: str) -> str:
+    return click.style(s, fg='blue')
+
+
+def CYAN(s: str) -> str:
+    return click.style(s, fg='cyan')
+
+
+def GREEN(s: str) -> str:
+    return click.style(s, fg='green')
+
+
+def RED(s: str) -> str:
+    return click.style(s, fg='red')
+
+
+def YELLOW(s: str) -> str:
+    return click.style(s, fg='yellow')
+
+
+def WHITE(s: str) -> str:
+    return click.style(s, fg='white')
+
+
+def NL() -> None:
+    click.echo('')
+
+
+def load_yaml(filepath: str) -> Any:
+    with open(filepath, 'r') as f:
+        return ruamel.yaml.safe_load(f)
+
+
+def save_yaml(data: Any, filepath: str) -> None:
+    with open(filepath, 'w') as f:
+        ruamel.yaml.dump(data, f)
+
+
+def validate_struct(data: Any, schema_name: Any) -> ValidationResult:
+    schema_filepath = pkg_resources.resource_filename('tft.artemis_cli', 'schemas/{}.yaml'.format(schema_name))
+    schema = load_yaml(schema_filepath)
+
+    try:
+        jsonschema.validate(instance=data, schema=schema)
+
+        return ValidationResult(True, [])
+
+    except jsonschema.exceptions.ValidationError:
+        validator = jsonschema.Draft4Validator(schema)
+
+        return ValidationResult(
+            False,
+            validator.iter_errors(data)
+        )
+
+
+def prettify_json(flag: bool, data: Any) -> str:
+    if not flag:
+        return json.dumps(data)
+
+    return json.dumps(data, sort_keys=True, indent=4)
+
+
+def prettify_yaml(flag: bool, data: Any) -> str:
+    Y = ruamel.yaml.YAML()
+
+    if flag:
+        Y.indent(sequence=2, mapping=2, offset=0)
+
+    stream = ruamel.yaml.compat.StringIO()
+
+    Y.dump(data, stream)
+
+    return cast(str, stream.getvalue())
+
+
+def execute_command(
+    cmd: List[str],
+    spinner: bool = False,
+    logger: Optional[Logger] = None,
+    accept_exit_codes: Optional[Iterable[int]] = None,
+    **kwargs: Any
+):
+    # type: (...) -> subprocess.CompletedProcess[bytes]
+
+    # add "accepted exit codes" when needed
+
+    accept_exit_codes = accept_exit_codes or [0]
+
+    logger = logger or Logger()
+
+    with click_spinner.spinner(disable=not spinner):
+        try:
+            result = subprocess.run(cmd, **kwargs)
+
+        except subprocess.SubprocessError as exc:
+            logger.error('Failed to complete command: {}'.format(exc))
+
+    if result.returncode not in accept_exit_codes:
+        logger.error("""
+Failed to complete command, exited with code {}:
+
+{}
+
+STDOUT: ---v---v---v---v---v---
+{}
+        ---^---^---^---^---^---
+
+STDERR: ---v---v---v---v---v---
+{}
+        ---^---^---^---^---^---
+""".format(
+            result.returncode,
+            shlex.quote(' '.join(cmd)),
+            result.stdout.decode('utf-8') if result.stdout else '',
+            result.stderr.decode('utf-8') if result.stderr else ''
+        ))
+
+    return result
+
+
+def fetch_remote(
+    url: str,
+    logger: Optional[Logger] = None,
+    spinner: bool = False,
+    method: str = 'get',
+    request_kwargs: Optional[Dict[str, Any]] = None,
+    on_error: Optional[Callable[[requests.Response], None]] = None
+) -> requests.Response:
+    logger = logger or Logger()
+    request_kwargs = request_kwargs or {}
+
+    url = urlnormalizer.normalize_url(url)
+
+    with click_spinner.spinner(disable=not spinner):
+        if method == 'get':
+            res = requests.get(url, **request_kwargs)
+
+        elif method == 'post':
+            res = requests.post(url, **request_kwargs)
+
+        elif method == 'delete':
+            res = requests.delete(url, **request_kwargs)
+
+    if res.status_code not in [200, 201]:
+        if on_error:
+            on_error(res)
+
+        else:
+            logger.error(
+                'Failed to communicate with remote url {}, responded with code {}'.format(url, res.status_code)
+            )
+
+    return res
+
+def fetch_artemis(cfg, endpoint, method='get', request_kwargs=None, logger=None):
+    assert cfg.artemis_api_url is not None
+    if not logger:
+        logger = Logger()
+    def _error_callback(res: requests.Response) -> None:
+        assert logger is not None
+
+        logger.error(
+                'Failed to communicate with Artemis API Server, responded with code {}: {}'.format(res.status_code, res.reason)
+        )
+
+
+    return fetch_remote('{}/{}'.format(cfg.artemis_api_url, endpoint), logger, method=method, request_kwargs=request_kwargs, on_error=_error_callback)
+
+def artemis_inspect(cfg, resource, rid, logger=None):
+    return fetch_artemis(cfg, '/{}/{}'.format(resource, rid), logger=None)
+
+def artemis_create(cfg, resource, data, logger=None):
+    return fetch_artemis(cfg, '/{}'.format(resource), method='post', request_kwargs={'json':data}, logger=None)
+
+def artemis_delete(cfg, resource, rid, logger=None):
+    return fetch_artemis(cfg, '{}/{}'.format(resource, rid), method='delete', logger=None)
+
+def confirm(
+    cfg: Configuration,
+    msg: str,
+    force: bool,
+    default: bool = False,
+    abort: bool = False,
+) -> Any:
+    """Wrapped click.confirm function to print to stderr."""
+    assert cfg.logger is not None
+
+    if force:
+        return force
+
+    if cfg.output_format == 'human':
+        click.echo(msg, nl=False)
+        return click.confirm('', default=default, abort=abort, err=True)
+
+    if abort:
+        cfg.logger.info(msg)
+        raise click.Abort()
+
+    return False
+
+
+def prompt(cfg: Configuration, msg: str, type: Any = None, default: Optional[str] = None) -> Any:
+    """Wrapped click.prompt function to print to stderr."""
+    assert cfg.logger is not None
+
+    if cfg.output_format == 'human':
+        click.echo(msg, nl=False)
+        return click.prompt('', type=type, default=default, err=True)
+
+    cfg.logger.error(
+        'click.prompt() unsupported in non-human output mode, please use the command-line options instead'
+    )
+
+
+def print_table(
+    table: List[List[str]],
+    format: str = 'text'
+) -> None:
+    def _to_items() -> List[Dict[str, str]]:
+        as_list = []
+
+        headers = table[0]
+
+        for row in table[1:]:
+            as_list.append({
+                header: cell for header, cell in zip(headers, row)
+            })
+
+        return as_list
+
+    if format == 'text':
+        printable = tabulate.tabulate(
+            table,
+            headers='firstrow',
+            tablefmt='psql'
+        )
+
+    elif format == 'json':
+        printable = prettify_json(True, _to_items())
+
+    elif format == 'yaml':
+        printable = prettify_yaml(True, _to_items())
+
+    else:
+        assert False, 'Table format {} is not supported'
+
+    click.echo(printable)
+
+
+JobReturnType = TypeVar('JobReturnType')
+JobCallbackType = Callable[..., JobReturnType]
+JobType = Tuple[JobCallbackType[JobReturnType], List[Any], Dict[str, Any]]
+
+
+def execute_jobs(
+    jobs: List[JobType[JobReturnType]],
+    max_workers: Optional[int] = None
+):
+    # type: (...) -> List[concurrent.futures.Future[JobReturnType]]
+
+    futures = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for callback, args, kwargs in jobs:
+            futures.append(executor.submit(callback, *args, **kwargs))
+
+        done, pending = concurrent.futures.wait(futures)
+
+        assert not pending
+
+    return futures
---
+++ cli/src/tft/artemis_cli/artemis_cli.py
@@ -0,0 +1,270 @@
+import os.path
+import requests
+import sys
+import tempfile
+import shutil
+import json
+
+import click
+import click_completion
+import stackprinter
+
+from tft import artemis_cli
+from . import Logger, Configuration, fetch_remote, NL, GREEN, RED, YELLOW, WHITE, prettify_json, \
+              artemis_inspect, artemis_create, artemis_delete, prompt, confirm
+
+from typing import cast, Any
+
+
+stackprinter.set_excepthook(
+    style='darkbg2',
+    source_lines=7,
+    show_signature=True,
+    show_vals='all',
+    reverse=False,
+    add_summary=False
+)
+
+click_completion.init()
+
+
+@click.group()
+@click.pass_context
+@click.option('--config', type=str, default=click.get_app_dir('artemis-cli'), help='Path to the configuration directory')
+def cli_root(ctx: Any, config: str) -> None:
+    ctx.ensure_object(Configuration)
+
+    cfg = cast(
+        Configuration,
+        ctx.obj
+    )
+
+    cfg.logger = Logger()
+    cfg.config_dirpath = os.path.expanduser(config)
+    cfg.config_filepath = os.path.join(cfg.config_dirpath, 'config.yaml')
+
+    if not os.path.exists(cfg.config_dirpath) or not os.path.exists(cfg.config_filepath):
+        if ctx.invoked_subcommand != 'init':
+            click.echo(RED(
+                'Config file {} does not exists, running configuration wizard'.format(cfg.config_filepath),
+            ))
+
+        ctx.invoke(cmd_init)
+        sys.exit(0)
+
+    cfg.raw_config = artemis_cli.load_yaml(cfg.config_filepath)
+
+    validation = artemis_cli.validate_struct(cfg.raw_config, 'config')
+
+    if not validation.result or cfg.raw_config is None:
+
+        click.echo(RED(
+            'Config file {} must be updated, found following validation errors:'.format(cfg.config_filepath),
+        ))
+
+        for error in validation.errors:
+            NL()
+            click.echo(RED(
+                '  * {}'.format(error.message)
+            ))
+            NL()
+        if cfg.raw_config is None:
+            NL()
+            click.echo(RED('Empty configuration file'))
+            NL()
+
+
+        click.echo(YELLOW(
+            'Running configuration wizard'
+        ))
+
+        ctx.invoke(cmd_init)
+        sys.exit(0)
+
+    cfg.artemis_api_url = cfg.raw_config['artemis_api_url']
+    assert cfg.artemis_api_url is not None
+
+
+@cli_root.group(name='guest', short_help='Guest related commands')
+@click.pass_obj
+def cmd_guest(cfg: Configuration) -> None:
+    pass
+
+@cmd_guest.command(name='create', short_help='Create provisioning request')
+@click.option('--keyname', required=True, help='name of ssh key')
+@click.option('--priority-group', help='name of priority group')
+@click.option('--arch', required=True, help='architecture')
+@click.option('--compose', help='compose id')
+@click.option('--beaker-distro', help='name of beaker distro')
+@click.option('--openstack-image', help='name of openstack image')
+@click.option('--aws-image', help='name of aws image')
+@click.option('--snapshots', is_flag=True, help='require snapshots support')
+@click.pass_obj
+def cmd_guest_create(
+        cfg: Configuration,
+        keyname: str = None,
+        arch: str = None,
+        compose = None,
+        beaker_distro = None,
+        openstack_image = None,
+        aws_image = None,
+        snapshots = False,
+        priority_group = None
+) -> None:
+    num_of_options = sum([ int(bool(o)) for o in [compose, beaker_distro, openstack_image, aws_image ]])
+    if num_of_options != 1:
+        Logger().error('Exactly one of these options is needed:\n'
+                       '  --compose OR --beaker-distro OR --openstack-image --OR aws-image\n'
+                       'provided: {}'.format(num_of_options))
+
+    environment = {}
+    environment['arch'] = arch
+    if compose:
+        environment['compose'] = {'id': compose}
+    elif beaker_distro:
+        environment['compose'] = {'beaker': {'distro': beaker_distro}}
+    elif openstack_image:
+        environment['compose'] = {'openstack': {'image': openstack_image}}
+    elif aws_image:
+        environment['compose'] = {'aws': {'image': aws_image}}
+    if snapshots:
+        environment['snapshots'] = True
+
+
+    data = {
+            'environment': environment,
+            'keyname': keyname,
+            'priority_group': 'default-priority'
+            }
+
+    response = artemis_create(cfg, 'guests/', data)
+    print(prettify_json(True, response.json()))
+
+
+@cmd_guest.command(name='inspect', short_help='Inspect provisioning request')
+@click.argument('guestname', metavar='ID', default=None,)
+@click.pass_obj
+#def cmd_guest_inspect(cfg: Configuration, guestname: str) -> None:
+def cmd_guest_inspect(cfg: Configuration, guestname: str) -> None:
+    response = artemis_inspect(cfg, 'guests', guestname)
+    print(prettify_json(True, response.json()))
+
+
+@cmd_guest.command(name='cancel', short_help='Cancel provisioning request')
+@click.argument('guestname', metavar='ID', default=None,)
+@click.pass_obj
+def cmd_cancel(cfg: Configuration, guestname: str) -> None:
+    logger=Logger()
+    response = artemis_delete(cfg, 'guests', guestname, logger=logger)
+    if response.ok:
+        logger.info('guest "{}" has been canceled'.format(guestname))
+
+
+@cmd_guest.command(name='list', short_help='List provisioning requests')
+@click.pass_obj
+def cmd_guest_list(cfg: Configuration) -> None:
+    response = artemis_inspect(cfg, 'guests', '')
+    print(prettify_json(True, response.json()))
+
+@cmd_guest.command(name='events', short_help='List event log')
+@click.argument('guestname', metavar='ID', default=None,)
+@click.pass_obj
+def cmd_guest_events(cfg: Configuration, guestname: str) -> None:
+    response = artemis_inspect(cfg, 'guests', '{}/events'.format(guestname))
+    print(prettify_json(True, response.json()))
+
+
+@cli_root.command(name='init', short_help='Initialize configuration file.')
+@click.pass_obj
+def cmd_init(cfg: Configuration) -> None:
+    """
+    Using a series of questions, initialize a configuration file. Some information can be extracted automatically,
+    other bits must be explicitly provided by the user.
+    """
+
+    # We try to use one way to do the same thing, including "printing to terminal". For that purpose,
+    # we have Logger class and cfg.logger and so on. But in this particular function, we want to print
+    # large pile of text, with colors, and that's more readable without logger. So, here's the exception...
+
+    # Oh God, how I miss the preprocessor ...
+    def TITLE(s: str) -> None:
+        click.echo(YELLOW(s))
+
+    def TEXT(s: str) -> None:
+        click.echo(WHITE(s))
+
+    def ERROR(s: str) -> None:
+        click.echo(RED(s))
+
+    def WARN(s: str) -> None:
+        click.echo(GREEN(s))
+
+    def SUCCESS(s: str) -> None:
+        click.echo(GREEN(s))
+
+    def QUESTION(s: str) -> str:
+        return YELLOW(s)
+
+    TEXT("""
+Hi! Following sequence of questions will help you setup configuration for this tool.
+
+Feel free to interrupt it anytime, nothing is saved until the very last step.
+""")
+
+    #
+    # Artemis API
+    #
+    TITLE('** Artemis API URL **')
+    TEXT("""
+URL of Artemis API, for example 'http://artemis.example.com'
+Currently artemis-cli needs just this URL.
+""")
+
+    artemis_api_url = prompt(
+        cfg,
+        QUESTION('Enter URL of Artemis API'),
+        type=str,
+        default=None
+    )
+
+    NL()
+
+    tmp_config_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+
+    with tmp_config_file:
+        print("""---
+
+artemis_api_url: {artemis_api_url}
+""".format(**locals()), file=tmp_config_file)
+
+        tmp_config_file.flush()
+
+    if confirm(
+        cfg,
+        QUESTION('Do you wish to check the configuration file, possibly modifying it?'),
+        False,
+        default=True
+    ):
+        click.edit(filename=tmp_config_file.name)
+
+    if confirm(
+        cfg,
+        QUESTION('Do you wish to save this as your configuration file?'),
+        False,
+        default=False
+    ):
+        assert cfg.config_dirpath is not None
+        assert cfg.config_filepath is not None
+
+        os.makedirs(cfg.config_dirpath, exist_ok=True)
+
+        if os.path.exists(cfg.config_filepath):
+            os.unlink(cfg.config_filepath)
+
+        shutil.copy(tmp_config_file.name, cfg.config_filepath)
+
+        SUCCESS('Saved, your config file has been updated')
+
+    else:
+        WARN('Ok, your answers were thrown away.')
+
