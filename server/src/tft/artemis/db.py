1b897f4ef4f419287be062f8a07f390f8f6821e7
Milos Prchlik <happz@happz.cz>
2020-09-14 21:33:39 +0200

    Adds database schema for multiuser support: user role and tokens
    Each user is assigned a role, ADMIN or USER, and each user has two
    tokens:
    
    * provisioning token, for actions related to guests and provisioning,
    * admin token, for other stuff, like adding new users.
    
    Instead of having a single token, I'd rather keep these two areas of
    possible API requests separated, as leaking a possible more accessible
    provisioning token wouldn't endanger the admin token.
    
    Only users with the ADMIN role will be allowed to exercise their
    administrative powers.

--- server/src/tft/artemis/db.py
+++ server/src/tft/artemis/db.py
@@ -1,9 +1,11 @@
 import dataclasses
 import datetime
 import enum
+import hashlib
 import json
 import logging
 import os
+import secrets
 import threading
 
 from contextlib import contextmanager
@@ -14,8 +16,13 @@ from sqlalchemy import BigInteger, Column, ForeignKey, String, Boolean, Enum, Te
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.query import Query as _Query
 
-from typing import cast, Any, Callable, Dict, Generic, Iterator, List, Optional, Type, TypeVar, Union
+from typing import cast, Any, Callable, Dict, Generic, Iterator, List, Optional, Tuple, Type, TypeVar, Union
 import gluetool.log
+
+
+# "A reasonable default" size of tokens. There's no need to tweak it in runtime, but we don't want
+# magic numbers spreading through our code.
+TOKEN_SIZE = 32
 
 
 Base = sqlalchemy.ext.declarative.declarative_base()
@@ -109,13 +116,77 @@ DEFAULT_SQLALCHEMY_POOL_SIZE = 20
 DEFAULT_SQLALCHEMY_MAX_OVERFLOW = 10
 
 
+class UserRoles(enum.Enum):
+    """
+    Possible user roles.
+    """
+
+    USER = 'USER'
+    ADMIN = 'ADMIN'
+
+
 class User(Base):
     __tablename__ = 'users'
 
     username = Column(String(250), primary_key=True, nullable=False)
 
+    role = Column(Enum(UserRoles), nullable=False, server_default=UserRoles.USER.value)
+    """
+    User role.
+    """
+
+    # Tokens are initialized to a short, human-readable string *on purpose*. We don't store the actual tokens
+    # in the database, only their SHA256 hashes, and there's no possible token whose hash would be "undefined".
+    # This makes newly created users safe from leaking any tokens by accident, user's tokens must be explicitly
+    # initialized by ADMIN-level account first.
+    admin_token = Column(String(TOKEN_SIZE), nullable=False, server_default='undefined')
+    """
+    Token used to authenticate actions not related to guests and provisioning. Stored as a SHA256
+    hash of the actual token.
+    """
+
+    provisioning_token = Column(String(TOKEN_SIZE), nullable=False, server_default='undefined')
+    """
+    Token used to authenticate actions related to guests and provisioning. Stored as a SHA256
+    hash of the actual token.
+    """
+
     sshkeys = relationship('SSHKey', back_populates='owner')
     guests = relationship('GuestRequest', back_populates='owner')
+
+    @staticmethod
+    def hash_token(token: str) -> str:
+        """
+        Returns a SHA256 hash of a given token, encoded as string of hexadecimal digits.
+        """
+
+        m = hashlib.sha256()
+        m.update(token.encode())
+
+        return m.hexdigest()
+
+    @staticmethod
+    def generate_token() -> Tuple[str, str]:
+        """
+        Generate new token. Returns a tuple of two items: the token and its hash.
+        """
+
+        token = secrets.token_hex(TOKEN_SIZE)
+
+        return token, User.hash_token(token)
+
+    @classmethod
+    def create(cls, username: str, role: UserRoles) -> 'User':
+        return cls(
+            username=username,
+            role=role.value
+        )
+
+    @classmethod
+    def fetch_by_username(cls, session: sqlalchemy.orm.session.Session, username: str) -> Optional['User']:
+        return Query.from_session(session, User) \
+               .filter(User.username == username) \
+               .one_or_none()
 
 
 class SSHKey(Base):
