d343e43b3ae20aa7b321da6a5d0ea760c6618053
Milos Prchlik <mprchlik@redhat.com>
2020-10-09 17:55:58 +0200

    Copy whole admonition content when refactoring it into a table
    The JS workaround to make admonitions nice and good looking was copying just the first <p> element into the newly created table. This makes some text disappear. This patch hopefully fixes that, and tries to copy all relevant child nodes into the table.

--- docs/static/js/fix-adoc.js
+++ docs/static/js/fix-adoc.js
@@ -10,8 +10,10 @@ window.addEventListener('load', function () {
     for (let i = admonitions.length - 1; i >= 0; i--) {
       const elm = admonitions[i]
       const type = elm.classList[1]
-      const text = elm.getElementsByTagName('p')[0].innerHTML
-      const parent = elm.parentNode
+
+      const parent = elm.parentNode;
+
+      // This is our temporary element, contains our newly constructed table for the admonition block.
       const tempDiv = document.createElement('div')
       tempDiv.innerHTML = `<div class="admonitionblock ${type}">
       <table>
@@ -21,14 +23,30 @@ window.addEventListener('load', function () {
               <i class="fa icon-${type}" title="${type}"></i>
             </td>
             <td class="content">
-              ${text}
             </td>
           </tr>
         </tbody>
       </table>
-    </div>`
-  
-      const input = tempDiv.childNodes[0]
-      parent.replaceChild(input, elm)
+      </div>`
+
+      // Now copy the content of the original admonition block into our a table cell for the content.
+      const content_td = tempDiv.getElementsByClassName('content')[0];
+
+      while (elm.hasChildNodes()) {
+        child = elm.removeChild(elm.firstChild);
+
+        // Do not copy <h6 /> elements: these are used by the admonition block to hold the block name,
+        // like "NOTE" or "WARNING" - but we don't need these anymore, as our new table shows an icon
+        // instead. And it's very unlikely we would ever use <h6 /> for anything real in these blocks.
+        if (child.tagName == 'H6') {
+          continue;
+        }
+
+        content_td.appendChild(child);
+      }
+
+      // And the final switch, replace original block with our table.
+      const input = tempDiv.childNodes[0];
+      parent.replaceChild(input, elm);
     }
-  })
+})
