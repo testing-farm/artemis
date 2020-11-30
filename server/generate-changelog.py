7ce776d56e21b118cfa4936164ce0b74533bd989
Milos Prchlik <mprchlik@redhat.com>
2020-11-30 21:47:41 +0100

    Adds a helper script for generating release changelog
    Our changelog follows https://keepachangelog.com/en/1.0.0/ guidelines.
    The script added by this patch is a small helper that will extract
    changelog entries from commits.
    
    For example, the changelog entries like this (prefixed with a space to
    avoid inclusion in the actual, real changelog later):
    
    ```
     Added: failure instances gained `recoverable` flag
     Fixed: comparison of actor names in middleware
     Added: beaker driver gained support for username/password credentials
     Added: "instance missing" failures marked as irrecoverable by OpenStack driver
     Changed: pool recource release decoupled from guest release
     Changed: drivers use DB representation of guests and snapshots internaly, instead of `Guest` instances
     Added: prototype if DB tests
     Removed: `safe_db_execute` replaced with `safe_db_change`
    ```
    
    After running the script, we get this changelog:
    
    ```markdown
     ## Added
    
     * failure instances gained `recoverable` flag
     * beaker driver gained support for username/password credentials
     * "instance missing" failures marked as irrecoverable by OpenStack
       driver
     * prototype if DB tests
    
     ## Changed
    
     * pool recource release decoupled from guest release
     * drivers use DB representation of guests and snapshots internaly,
       instead of `Guest` instances
    
     ## Removed
    
     * `safe_db_execute` replaced with `safe_db_change`
    
     ## Fixed
    
     * comparison of actor names in middleware
    ```
    
    Which is the output we can easily insert into release notes.

---
+++ server/generate-changelog.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+import jinja2
+
+from typing import Dict, List
+
+
+USAGE = """
+git log ... | generate-changelog.py [-h|--help]
+
+Pipe an output of `git log` command to this script, to get changelog entries, according to
+https://keepachangelog.com/en/1.0.0/.
+
+For example:
+
+$ git log <last version tag>..master | poetry run ./generate-changelog.py
+$ git show | poetry run ./generate-changelog.py
+"""
+
+
+SECTION_NAMES = [
+    'Added',
+    'Changed',
+    'Deprecated',
+    'Removed',
+    'Fixed',
+    'Security'
+]
+
+
+SECTION_PATTERNS = {
+    re.compile(rf'(?i)^{section_name.lower()}\s*:\s*.+$'): section_name
+    for section_name in SECTION_NAMES
+}
+
+
+SECTION_TEMPLATE = jinja2.Template("""
+## {{ SECTION_NAME }}
+{% for item in SECTION_ITEMS %}
+* {{ item }}
+{%- endfor %}
+""")
+
+
+def usage() -> None:
+    print(USAGE)
+
+
+def main() -> None:
+    section_items: Dict[str, List[str]] = {
+        section_name: []
+        for section_name in SECTION_NAMES
+    }
+
+    for line in sys.stdin:
+        line = line.strip()
+
+        for pattern, section_name in SECTION_PATTERNS.items():
+            if not pattern.match(line):
+                continue
+
+            section_items[section_name].append(line.split(':', 1)[1].strip())
+
+    for name, items in section_items.items():
+        if not items:
+            continue
+
+        print(SECTION_TEMPLATE.render(SECTION_NAME=name, SECTION_ITEMS=items).strip())
+        print()
+        print()
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 2 and sys.argv[1].lower() in ('-h', '--help')) or sys.stdin.isatty():
+        usage()
+
+        sys.exit(0)
+
+    main()
