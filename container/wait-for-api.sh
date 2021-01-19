68f9a9ccbe6c44ec804095b738af1e5aff493f03
Miroslav Vadkerti <mvadkert@redhat.com>
2021-01-19 11:27:20 +0100

    container/wait-for-api.sh: make the api timeout longer
    And configurable. We are hitting this in staging artemis now ...
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- container/wait-for-api.sh
+++ container/wait-for-api.sh
@@ -7,11 +7,12 @@ error() { echo "$@"; exit 1; }
 
 [ -z "$API_HOSTNAME" ] && error "API_HOSTNAME environment variable not set"
 [ -z "$API_PORT" ] && error "API_PORT environment variable not set"
+[ -z "$API_TIMEOUT" ] && API_TIMOEUT=10
 [ -z "$WAIT_TIMEOUT" ] && WAIT_TIMEOUT=60
 [ -z "$WAIT_TICK" ] && WAIT_TICK=1
 
 time=0
-until timeout 1 curl -sf http://$API_HOSTNAME:$API_PORT/_docs
+until timeout $API_TIMEOUT curl -sf http://$API_HOSTNAME:$API_PORT/_docs
 do
     time=$((time + WAIT_TICK))
     [ $time -ge $WAIT_TIMEOUT ] && error "Failed to wait for api to start"
