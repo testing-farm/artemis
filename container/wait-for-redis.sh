43cbc160f730e2cd7006d26d898a6528fd65e2fa
Milos Prchlik <mprchlik@redhat.com>
2021-02-23 20:40:41 +0100

    Adds a "wait for Redis" script into container

---
+++ container/wait-for-redis.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+#
+# Script waits for redis availability
+#
+
+error() { echo "$@"; exit 1; }
+
+[ -z "$REDIS_HOSTNAME" ] && error "REDIS_HOSTNAME environment variable not set"
+[ -z "$REDIS_PORT" ] && error "REDIS_PORT environment variable not set"
+[ -z "$WAIT_TIMEOUT" ] && WAIT_TIMEOUT=60
+[ -z "$WAIT_TICK" ] && WAIT_TICK=1
+
+check_redis() {
+    timeout 1 (printf "PING\r\n";) | nc -w 1 127.0.0.1 6379 | grep -q PONG
+    return $?
+}
+
+time=0
+until check_redis; do
+    time=$((time + WAIT_TICK))
+    [ $time -ge $WAIT_TIMEOUT ] && error "Failed to wait for redis to start"
+    sleep $WAIT_TICK
+done
