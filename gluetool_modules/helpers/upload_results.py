8cf7a46a2e9448d4f480eb852bcd04395270b5b4
Jakub Haruda <jharuda@redhat.com>
2021-06-04 17:35:13 +0200

    upload-results: New module for linux-system-roles

---
+++ gluetool_modules/helpers/upload_results.py
@@ -0,0 +1,238 @@
+import os
+from datetime import datetime
+
+import gluetool
+from gluetool import Failure
+from gluetool import GlueCommandError
+from gluetool import GlueError
+from gluetool.utils import Command
+
+from typing import AnyStr, List, Optional # noqa
+
+
+class UploadResults(gluetool.Module):
+    """
+    This module is for uploading test results in linux-system-roles BaseOS CI use-case.
+    It does not provide generic functionality for gluetool-module.
+
+    It is used at the end of the citool pipeline.
+
+    It uses entries in ``test_schedule`` as a source of artifacts.
+    It provides ``PR_TESTING_ARTIFACTS_URL`` as the target of uploaded results on the web.
+    """
+
+    name = 'upload-results'
+    description = 'Upload result using scp'
+
+    supported_dryrun_level = gluetool.glue.DryRunLevels.DRY
+
+    options = {
+        'artifact-src-filenames': {
+            'help': 'The filenames of source artifacts we want to upload',
+            'metavar': 'path',
+            'type': str
+        },
+        'artifact-dest-file-postfix': {
+            'help': 'The postfix in the end of the uploaded test results filename.',
+            'metavar': 'path',
+            'type': str
+        },
+        'artifact-target-dir-name': {
+            'help': 'The name of a directory for artifacts in `target-dir`',
+            'metavar': 'path',
+            'type': str
+        },
+        'artifact-target-subdirs': {
+            'help': 'The subdirectories in `target-dir`/`artifact_target-dir-name` where to upload results. Optional',
+            'metavar': 'path',
+            'type': str
+        },
+        'key-path': {
+            'help': 'the path to the key which will be used to upload',
+            'metavar': 'path',
+            'type': str
+        },
+        'upload-to-public': {
+            'help': 'Uploads results to public space if set',
+            'action': 'store_true'
+        },
+        'user': {
+            'help': 'The user which will be used by scp to log in target host',
+            'metavar': 'USER',
+            'type': str
+        },
+        'domain': {
+            'help': 'The domain to which results will be uploaded',
+            'metavar': 'URL',
+            'type': str
+        },
+        'target-url': {
+            'help': 'The URL to which results will be uploaded',
+            'metavar': 'URL',
+            'type': str
+        },
+        'target-dir': {
+            'help': 'The directory in target host where artifacts will be uploaded',
+            'metavar': 'PATH',
+            'type': str
+        }
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(UploadResults, self).__init__(*args, **kwargs)
+        self.full_target_url = None
+
+    def _get_pull_request_info(self):
+        # type: () -> str
+        """
+        It generates a string from pull request information.
+
+        :rtype: str
+        :returns: Formated pull request info.
+        """
+        task = self.shared('primary_task')
+        return "{}-{}-{}".format(task.repo, task.pull_number, task.commit_sha[0:7])
+
+    def _get_artifact_dir_name(self):
+        # type: () -> str
+        """
+        It generates a name for the results folder.
+
+        :rtype: str
+        :returns: The name of the folder where the results will be uploaded
+        """
+        artifact_folder_name = self.option('artifact-target-dir-name').format(
+            self._get_pull_request_info(),
+            datetime.now().strftime('%Y%m%d-%H%M%S')
+        )
+        return artifact_folder_name
+
+    def _create_subdir_for_artifacts(self, destination_sub_path, user_and_domain):
+        # type: (str, str) -> str
+        """
+        This will create a folder for the results on the target file hosting.
+
+        :param str destination_sub_path: Main destination path in filesystem for results.
+        :param str user_and_domain: User login to the server.
+        """
+        target_subdirectory = self.option('artifact-target-subdirs')
+        if target_subdirectory:
+            destination_sub_path = "{}/{}".format(destination_sub_path, target_subdirectory)
+            target_dir = self.option('target-dir')
+            cmd_init_remote_dir = [
+                'ssh', '-i', self.option('key-path'),
+                user_and_domain,
+                "mkdir -p {}".format(os.path.join(target_dir, destination_sub_path))
+            ]
+            try:
+                Command(cmd_init_remote_dir).run()
+                return destination_sub_path
+            except GlueCommandError as exc:
+                assert exc.output.stderr is not None
+                raise GlueError('Creating remote folder failed: {} cmd: {}'.format(exc, cmd_init_remote_dir))
+
+    def _get_files_to_upload(self):
+        # type: () -> List(dict(str, str), dict(str, str))
+        """
+        Get the results to be uploaded to the server.
+
+        :returns: The source paths to the test results and destination filenames.
+        """
+        schedule = self.shared('test_schedule')
+        dest_file_postfix = self.option('artifact-dest-file-postfix')
+
+        files = []
+        for entry in schedule:
+            dest_filename = "{}-{}{}".format(
+                os.path.splitext(
+                    entry.playbook_filepath.split('/')[-1]
+                )[0],
+                entry.result,
+                dest_file_postfix
+            )
+
+            files.append({
+                'src-file-path': os.path.join(entry.work_dirpath, self.option('artifact-src-filenames')),
+                'dest-filename': dest_filename
+            })
+
+        return files
+
+    def _upload_results(self, destination_path, user_and_domain, results_files):
+        # type: (str, str, List(dict(str, str), dict(str, str))) -> None
+        """
+        It uploads the artifacts to the server.
+
+        :param str destination_path: Where to upload results. Example: ``/data/logs/result1/``
+        :param str user_and_domain: User login to the server. Example: ``root@domain.com``
+        :param dict results_files: Full paths to the source artifacts and destination filenames.
+        """
+        for results_file in results_files:
+            cmd_upload = ['scp', '-i', self.option('key-path')]
+
+            cmd_upload.append(results_file['src-file-path'])
+
+            cmd_upload.append('{}:{}'.format(
+                user_and_domain,
+                os.path.join(destination_path, results_file['dest-filename'])
+            ))
+
+            try:
+                Command(cmd_upload).run()
+                cmd_upload = None
+            except GlueCommandError as exc:
+                assert exc.output.stderr is not None
+                raise GlueError('Uploading results failed: {} cmd: {}'.format(exc, cmd_upload))
+
+    @property
+    def _full_target_url(self):
+        # type: () -> str
+        return self.full_target_url
+
+    @property
+    def eval_context(self):
+        # type: () -> str
+        __content__ = { # noqa
+            'PR_TESTING_ARTIFACTS_URL': """
+                          The URL with results of testing
+                          """
+        }
+        return {
+            'PR_TESTING_ARTIFACTS_URL': self._full_target_url
+        }
+
+    def destroy(self, failure=None):
+        # type: (Optional[Failure]) -> None
+        """
+        It creates a directory for results in destination and then it uploads test results.
+        At the end ``PR_TESTING_ARTIFACTS_URL`` contains the URL with the uploaded results.
+
+        :param gluetool.glue.Failure failure: if set, carries information about failure that made
+          ``gluetool`` to destroy the whole session. Modules might want to take actions based
+          on provided information, e.g. send different notifications.
+        """
+        if not self.shared('test_schedule'):
+            raise gluetool.GlueError('test_shedule is empty.')
+
+        if not self.option('upload-to-public'):
+            return
+
+        domain = self.option('domain')
+        user = self.option('user')
+        user_and_domain = "{}@{}".format(user, domain)
+
+        destination_sub_path = self._get_artifact_dir_name()
+
+        destination_sub_path = self._create_subdir_for_artifacts(destination_sub_path, user_and_domain)
+
+        target_url = self.option('target-url')
+        self.destination_url = os.path.join(target_url, destination_sub_path)
+
+        target_dir = self.option('target-dir')
+        self.destination_dir = os.path.join(target_dir, destination_sub_path)
+
+        # Return artifacts URL
+        self.full_target_url = "https://{}/{}".format(domain, self.destination_url)
+
+        files = self._get_files_to_upload()
+        self._upload_results(self.destination_dir, user_and_domain, files)
---
+++ gluetool_modules/tests/test_upload_results.py
@@ -0,0 +1,174 @@
+import pytest
+
+from mock import MagicMock, call
+
+import gluetool
+import gluetool_modules.helpers.upload_results
+from gluetool_modules.helpers import upload_results
+
+from . import check_loadable, create_module, patch_shared
+
+
+@pytest.fixture(name='module')
+def fixture_module():
+    module = create_module(upload_results.UploadResults)[1]
+    module._config['artifact-src-filenames'] = 'ansible.out'
+    module._config['artifact-dest-file-postfix'] = '.log'
+    module._config['artifact-target-dir-name'] = 'pull_request3_fs45da5'
+    module._config['artifact-target-subdirs'] = 'artifacts'
+    module._config['key-path'] = '/home/user/.ssh/id_rsa'
+    module._config['upload-to-public'] = True
+    module._config['user'] = 'user'
+    module._config['domain'] = 'myGlueResults.com'
+    module._config['target-url'] = 'artifacts/results'
+    module._config['target-dir'] = 'data/artifacts/results'
+    return module
+
+
+def test_loadable(module):
+    check_loadable(module.glue, 'gluetool_modules/helpers/upload_results.py', 'UploadResults')
+
+
+def test_upload_result(module, monkeypatch):
+    mock_runinfo = MagicMock()
+    mock_runinfo.stdout = ''
+    mock_runinfo.stderr = ''
+    mock_command_run = MagicMock(return_value=mock_runinfo)
+
+    mock_command = MagicMock(return_value=MagicMock(run=mock_command_run))
+
+    monkeypatch.setattr(gluetool_modules.helpers.upload_results, 'Command', mock_command)
+
+    destination_path = 'dest/foo/bar'
+    user_and_domain = 'user@domain'
+    files = [{'src-file-path': 'src/foo/bar/ansible.log', 'dest-filename': 'foo.log'}]
+    module._upload_results(destination_path, user_and_domain, files)
+
+    mock_command.assert_called_with([
+        'scp',
+        '-i',
+        '/home/user/.ssh/id_rsa',
+        'src/foo/bar/ansible.log',
+        'user@domain:dest/foo/bar/foo.log'
+    ])
+
+
+def test_upload_multiple_results(module, monkeypatch):
+    mock_runinfo = MagicMock()
+    mock_runinfo.stdout = ''
+    mock_runinfo.stderr = ''
+    mock_command_run = MagicMock(return_value=mock_runinfo)
+
+    mock_command = MagicMock(return_value=MagicMock(run=mock_command_run))
+
+    monkeypatch.setattr(gluetool_modules.helpers.upload_results, 'Command', mock_command)
+
+    destination_path = 'dest/foo/bar'
+    user_and_domain = 'user@domain'
+    files = [
+        {'src-file-path': 'src/foo/bar/ansible.log', 'dest-filename': 'foo.log'},
+        {'src-file-path': 'src/foo2/bar2/ansible2.log', 'dest-filename': 'foo2.log'}
+    ]
+    module._upload_results(destination_path, user_and_domain, files)
+
+    mock_command.assert_has_calls([
+        call([
+            'scp',
+            '-i',
+            '/home/user/.ssh/id_rsa',
+            'src/foo/bar/ansible.log',
+            'user@domain:dest/foo/bar/foo.log']),
+        call([
+            'scp',
+            '-i',
+            '/home/user/.ssh/id_rsa',
+            'src/foo2/bar2/ansible2.log',
+            'user@domain:dest/foo/bar/foo2.log'])
+    ])
+
+
+def test_destroy(module, monkeypatch):
+    mock_runinfo = MagicMock()
+    mock_runinfo.stdout = ''
+    mock_runinfo.stderr = ''
+    mock_command_run = MagicMock(return_value=mock_runinfo)
+
+    mock_command = MagicMock(return_value=MagicMock(run=mock_command_run))
+
+    monkeypatch.setattr(gluetool_modules.helpers.upload_results, 'Command', mock_command)
+
+    mock_primary_task = MagicMock()
+    mock_primary_task.repo = 'foo-repo'
+    mock_primary_task.pull_number = 42
+    mock_primary_task.commit_sha = 'a1b0c3d4'
+
+    mock_test_entry_1 = MagicMock()
+    mock_test_entry_1.work_dirpath = 'work/dir/path'
+    mock_test_entry_1.playbook_filepath = 'repo/tests/test_foo.yml'
+    mock_test_entry_1.result = 'PASSED'
+
+    mock_test_entry_2 = MagicMock()
+    mock_test_entry_2.work_dirpath = 'work/dir2/path2'
+    mock_test_entry_2.playbook_filepath = 'repo/tests/test_bar.yml'
+    mock_test_entry_2.result = 'PASSED'
+
+    mock_test_schedule = [mock_test_entry_1, mock_test_entry_2]
+
+    patch_shared(monkeypatch, module, {
+        'primary_task': mock_primary_task,
+        'test_schedule': mock_test_schedule
+    })
+
+    module.destroy()
+
+    mock_command.assert_has_calls([
+        call([
+            'ssh',
+            '-i',
+            '/home/user/.ssh/id_rsa',
+            'user@myGlueResults.com',
+            'mkdir -p data/artifacts/results/pull_request3_fs45da5/artifacts'
+        ]),
+        call([
+            'scp',
+            '-i',
+            '/home/user/.ssh/id_rsa',
+            'work/dir/path/ansible.out',
+            'user@myGlueResults.com:data/artifacts/results/pull_request3_fs45da5/artifacts/test_foo-PASSED.log']),
+        call([
+            'scp',
+            '-i',
+            '/home/user/.ssh/id_rsa',
+            'work/dir2/path2/ansible.out',
+            'user@myGlueResults.com:data/artifacts/results/pull_request3_fs45da5/artifacts/test_bar-PASSED.log'])
+    ])
+
+    assert module.full_target_url == 'https://myGlueResults.com/artifacts/results/pull_request3_fs45da5/artifacts'
+
+
+def test_destroy_empty_schedule(module, monkeypatch):
+    mock_runinfo = MagicMock()
+    mock_runinfo.stdout = ''
+    mock_runinfo.stderr = ''
+    mock_command_run = MagicMock(return_value=mock_runinfo)
+
+    mock_command = MagicMock(return_value=MagicMock(run=mock_command_run))
+
+    monkeypatch.setattr(gluetool_modules.helpers.upload_results, 'Command', mock_command)
+
+    mock_primary_task = MagicMock()
+    mock_primary_task.repo = 'foo-repo'
+    mock_primary_task.pull_number = 42
+    mock_primary_task.commit_sha = 'a1b0c3d4'
+
+    mock_test_schedule = []
+
+    patch_shared(monkeypatch, module, {
+        'primary_task': mock_primary_task,
+        'test_schedule': mock_test_schedule
+    })
+
+    with pytest.raises(gluetool.GlueError, match=r"^test_shedule is empty.$"):
+        module.destroy()
+
+    mock_command.assert_not_called()
