528b22397962ce279bd476f609d9fd9bc767e67c
Miloš Prchlík <mprchlik@redhat.com>
2022-01-13 17:15:57 +0100

    Adds support for enlarging block device mappings list as needed

--- server/tests/drivers/test_aws.py
+++ server/tests/drivers/test_aws.py
@@ -1,6 +1,7 @@
 # Copyright Contributors to the Testing Farm project.
 # SPDX-License-Identifier: Apache-2.0
 
+import _pytest.monkeypatch
 
 import tft.artemis.drivers.aws
 
@@ -58,3 +59,39 @@ def test_bdm_find_free_device_name() -> None:
 
     assert r_name.is_ok
     assert r_name.unwrap() == '/dev/sdh'
+
+
+def test_bdm_find_free_device_name_exhausted(monkeypatch: _pytest.monkeypatch.MonkeyPatch) -> None:
+    mappings = tft.artemis.drivers.aws.BlockDeviceMappings()
+    monkeypatch.setattr(tft.artemis.drivers.aws, 'EBS_DEVICE_NAMES', [])
+
+    r = mappings.find_free_device_name()
+
+    assert r.is_error
+    assert r.unwrap_error().message == 'cannot find any free EBS device name'
+
+
+def test_bdm_enlarge() -> None:
+    mappings = tft.artemis.drivers.aws.BlockDeviceMappings()
+
+    assert len(mappings) == 0
+
+    assert mappings.enlarge(5).is_ok
+
+    assert len(mappings) == 5
+
+    assert mappings.enlarge(4).is_ok
+
+    assert len(mappings) == 5
+
+
+def test_bdm_enlarge_exhausted_names(monkeypatch: _pytest.monkeypatch.MonkeyPatch) -> None:
+    mappings = tft.artemis.drivers.aws.BlockDeviceMappings()
+    monkeypatch.setattr(tft.artemis.drivers.aws, 'EBS_DEVICE_NAMES', [])
+
+    r = mappings.enlarge(5)
+
+    assert r.is_error
+    assert r.unwrap_error().message == 'cannot find any free EBS device name'
+
+    assert len(mappings) == 0
