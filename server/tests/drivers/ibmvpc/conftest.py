e80db56ed7fd319da2be5372a4a73b3f5c44bb34
Miloš Prchlík <happz@happz.cz>
2025-01-30 16:22:07 +0000

    Sanitize also length of tags in IBM VPC driver

---
+++ server/tests/drivers/ibmvpc/conftest.py
@@ -0,0 +1,16 @@
+# Copyright Contributors to the Testing Farm project.
+# SPDX-License-Identifier: Apache-2.0
+
+import gluetool.log
+import pytest
+
+import tft.artemis.drivers.ibmcloudvpc
+
+
+@pytest.fixture(name='ibmvpc_pool', scope='function')
+def fixture_ibmvpc_pool(logger: gluetool.log.ContextAdapter) -> tft.artemis.drivers.ibmcloudvpc.IBMCloudVPCDriver:
+    return tft.artemis.drivers.ibmcloudvpc.IBMCloudVPCDriver(
+        logger,
+        'ibmcloud-vpc',
+        {}
+    )
