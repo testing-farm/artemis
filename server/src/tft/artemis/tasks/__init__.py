5e4045d9ffc34beb96dcdbdcf76b731a73d0a560
Miloš Prchlík <mprchlik@redhat.com>
2025-04-02 22:45:52 +0200

    Make entered/finished task events optional

--- server/src/tft/artemis/tasks/__init__.py
+++ server/src/tft/artemis/tasks/__init__.py
@@ -301,6 +301,15 @@ KNOB_OFFLOAD_TASKS: Knob[bool] = Knob(
     default=True
 )
 
+KNOB_TRACE_TASKS_AS_EVENTS: Knob[bool] = Knob(
+    'actor.trace-tasks-as-events',
+    'When enabled, each task will emit "entered/finished task" event.',
+    has_db=False,
+    envvar='ARTEMIS_TRACE_TASKS_AS_EVENTS',
+    cast_from_str=gluetool.utils.normalize_bool_option,
+    default=True
+)
+
 POOL_DRIVERS = {
     'aws': aws_driver.AWSDriver,
     'beaker': beaker_driver.BeakerDriver,
@@ -2013,7 +2022,11 @@ class Workspace:
 
     def _begin(self) -> None:
         if self.guestname:
-            self._guest_request_event('entered-task')
+            if KNOB_TRACE_TASKS_AS_EVENTS.value:
+                self._guest_request_event('entered-task')
+
+            else:
+                self._event('entered-task')
 
         else:
             self._event('entered-task')
@@ -2033,7 +2046,11 @@ class Workspace:
 
     def _complete(self) -> None:
         if self.guestname:
-            self._guest_request_event('finished-task')
+            if KNOB_TRACE_TASKS_AS_EVENTS.value:
+                self._guest_request_event('finished-task')
+
+            else:
+                self._event('finished-task')
 
         else:
             self._event('finished-task')
@@ -2049,7 +2066,11 @@ class Workspace:
 
     def _reschedule(self) -> None:
         if self.guestname:
-            self._guest_request_event('rescheduled')
+            if KNOB_TRACE_TASKS_AS_EVENTS.value:
+                self._guest_request_event('rescheduled')
+
+            else:
+                self._event('rescheduled')
 
         else:
             self._event('rescheduled')
