51cbb7b9a59c936b0a83f839f855346cc2b43e03
siwalter@redhat.com <siwalter@redhat.com>
2025-07-21 17:56:35 +0000

    better can_acquire rejection handling; URLs get pool name added

--- server/src/tft/artemis/drivers/flasher.py
+++ server/src/tft/artemis/drivers/flasher.py
@@ -1,7 +1,6 @@
 # Copyright Contributors to the Testing Farm project.
 # SPDX-License-Identifier: Apache-2.0
 
-import base64
 import dataclasses
 import json
 from typing import Any, Dict, Optional
@@ -14,7 +13,7 @@ import sqlalchemy.orm.session
 from gluetool.result import Error, Ok, Result
 
 from .. import Failure
-from ..db import GuestLog, GuestLogContentType, GuestLogState, GuestRequest
+from ..db import GuestLog, GuestLogContentType, GuestRequest
 from ..knobs import KNOB_DISABLE_CERT_VERIFICATION, KNOB_HTTP_TIMEOUT
 from ..metrics import PoolResourcesMetrics
 from . import (
@@ -82,28 +81,6 @@ class FlasherDriver(PoolDriver):
         session: sqlalchemy.orm.session.Session,
         guest_request: GuestRequest
     ) -> Result[CanAcquire, Failure]:
-        '''
-        Request
-        """""""
-
-        .. code-block:: json
-
-           GET /guests
-
-           {
-             "environment": guest request environment
-           }
-
-        Response
-        """"""""
-
-        .. code-block:: json
-
-           {
-             "result": boolean,
-             "reason": optional[string]
-           }
-        '''
         r_answer = super().can_acquire(logger, session, guest_request)
 
         if r_answer.is_error:
@@ -116,29 +93,31 @@ class FlasherDriver(PoolDriver):
             return Ok(CanAcquire.cannot('kickstart not supported'))
 
         payload = {
-            "environment": base64.b64encode(json.dumps(guest_request._environment).encode()),
+            "environment": guest_request._environment,
+            "metadata": json.dumps(guest_request.user_data),
         }
 
         try:
-            response = requests.get(
-                f"{self.url}/guests",
-                params=payload,
+            response = requests.post(
+                f"{self.url}/{self.poolname}/precheck",
+                json=payload,
                 verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
                 timeout=KNOB_HTTP_TIMEOUT.value
             )
-            response.raise_for_status()
         except requests.exceptions.RequestException as exc:
             return Error(Failure.from_exc(
                 'failed to query acquisition',
                 exc
             ))
 
-        data = response.json()
+        can_acquire = False
+        reason = None
+        if response.status_code == 200:
+            can_acquire = True
+        else:
+            reason = Failure(message=response.content, recoverable=self._is_recoverable(response))
 
-        result = data.get("result")
-        reason = data.get("reason", None)
-
-        return Ok(CanAcquire(can_acquire=result, reason=Failure(reason) if reason else None))
+        return Ok(CanAcquire(can_acquire=can_acquire, reason=reason))
 
     def acquire_guest(
         self,
@@ -182,7 +161,7 @@ class FlasherDriver(PoolDriver):
 
         try:
             response = requests.post(
-                f"{self.url}/guests",
+                f"{self.url}/{self.poolname}/guests",
                 json=payload,
                 headers=self._get_headers(guestname=guest_request.guestname),
                 verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
@@ -251,7 +230,7 @@ class FlasherDriver(PoolDriver):
 
         try:
             response = requests.get(
-                f"{self.url}/guests/{pool_data.guest_id}",
+                f"{self.url}/{self.poolname}/guests/{pool_data.guest_id}",
                 json=payload,
                 headers=self._get_headers(guestname=guest_request.guestname),
                 verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
@@ -322,7 +301,7 @@ class FlasherDriver(PoolDriver):
         pool_resources = FlasherPoolResourcesIDs.unserialize_from_json(raw_resource_ids)
         try:
             response = requests.delete(
-                f"{self.url}/guests/{pool_resources.guest_id}",
+                f"{self.url}/{self.poolname}/guests/{pool_resources.guest_id}",
                 headers=self._get_headers(guestname=pool_resources.guestname),
                 verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
                 timeout=KNOB_HTTP_TIMEOUT.value
@@ -373,7 +352,7 @@ class FlasherDriver(PoolDriver):
 
         try:
             response = requests.get(
-                f"{self.url}/pool_resources_metrics",
+                f"{self.url}/{self.poolname}/pool_resources_metrics",
                 verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
                 timeout=KNOB_HTTP_TIMEOUT.value
             )
@@ -394,6 +373,22 @@ class FlasherDriver(PoolDriver):
 
         return Ok(resources)
 
+
+    def _is_recoverable(self, response: requests.Response) -> bool:
+        code = response.status_code
+        if code == 500:
+            return False
+        if code == 501:
+            return False
+        if code == 400:
+            return False
+        if code == 404:
+            return True
+        if code == 503:
+            return True
+        response.raise_for_status()
+
+
     def _get_guest_log_url(
         self,
         guest_request: GuestRequest,
@@ -402,7 +397,6 @@ class FlasherDriver(PoolDriver):
         """
         Create location (URL) of guest log.
 
-        :param logger: logger to use for logging.
         :param guest_request: a request whose logs to look for.
         :param log_name: a name of the log as known to backend (e.g. ``cmd/latest``).
         :returns: log URL.
@@ -410,7 +404,7 @@ class FlasherDriver(PoolDriver):
 
         pool_data = guest_request.pool_data.mine(self, FlasherPoolData)
 
-        return f"{self.url}/getlog/{pool_data.guest_id}/{log_name}"
+        return f"{self.url}/{self.poolname}/getlog/{pool_data.guest_id}/{log_name}"
 
     def _update_guest_log_blob(
         self,
@@ -428,7 +422,6 @@ class FlasherDriver(PoolDriver):
                                     verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
                                     timeout=KNOB_HTTP_TIMEOUT.value)
             response.raise_for_status()
-
         except requests.exceptions.RequestException as exc:
             return Error(Failure.from_exc(
                 'failed to fetch flasher log',
@@ -441,32 +434,6 @@ class FlasherDriver(PoolDriver):
             guest_log,
             response.text
         ))
-
-    @guest_log_updater('flasher', 'flasher-debug:dump', GuestLogContentType.URL)  # type: ignore[arg-type]
-    def _update_guest_log_cmd_all_url(
-        self,
-        logger: gluetool.log.ContextAdapter,
-        guest_request: GuestRequest,
-        guest_log: GuestLog
-    ) -> Result[GuestLogUpdateProgress, Failure]:
-        url = self._get_guest_log_url(guest_request, 'cmd/all')
-        return Ok(GuestLogUpdateProgress(state=GuestLogState.COMPLETE, url=url))
-
-    @guest_log_updater('flasher', 'flasher-event:dump', GuestLogContentType.URL)  # type: ignore[arg-type]
-    def _update_guest_log_event_url(
-        self,
-        logger: gluetool.log.ContextAdapter,
-        guest_request: GuestRequest,
-        guest_log: GuestLog
-    ) -> Result[GuestLogUpdateProgress, Failure]:
-        """
-        This log contains explicit logging output and does not contain much debug output. It will show the flow of
-        events taking place while provisioning a guest. It can be compared to the Artemis guest event log and is a good
-        place to look for where the problem occurred. Why the problem occurred could be discovered in the 'cmd' log,
-        which contains output of the underlying commands being executed to provision the guest.
-        """
-        url = self._get_guest_log_url(guest_request, 'event')
-        return Ok(GuestLogUpdateProgress(state=GuestLogState.COMPLETE, url=url))
 
     @guest_log_updater('flasher', 'flasher-debug:dump', GuestLogContentType.BLOB)  # type: ignore[arg-type]
     def _update_guest_log_cmd_blob(
@@ -492,6 +459,12 @@ class FlasherDriver(PoolDriver):
         guest_request: GuestRequest,
         guest_log: GuestLog
     ) -> Result[GuestLogUpdateProgress, Failure]:
+        """
+        This log contains explicit logging output and does not contain much debug output. It will show the flow of
+        events taking place while provisioning a guest. It can be compared to the Artemis guest event log and is a good
+        place to look for where the problem occurred. Why the problem occurred could be discovered in the 'cmd' log,
+        which contains output of the underlying commands being executed to provision the guest.
+        """
         return self._update_guest_log_blob(
             logger,
             guest_log,
@@ -541,7 +514,7 @@ class FlasherDriver(PoolDriver):
 
         try:
             response = requests.post(
-                f"{self.url}/guests/{pool_data.guest_id}/reboot",
+                f"{self.url}/{self.poolname}/guests/{pool_data.guest_id}/reboot",
                 headers=self._get_headers(guestname=guest_request.guestname),
                 verify=not KNOB_DISABLE_CERT_VERIFICATION.value,
                 timeout=KNOB_HTTP_TIMEOUT.value
