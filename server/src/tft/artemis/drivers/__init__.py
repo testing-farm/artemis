37546c1b24cebc4947b88d593bae48980140ebe9
ina vasilevskaya <inavasilevskaya@gmail.com>
2025-12-09 15:36:21 +0000

    Calculate real ibmcloud processors count from flavor

--- server/src/tft/artemis/drivers/__init__.py
+++ server/src/tft/artemis/drivers/__init__.py
@@ -100,6 +100,7 @@ ConfigFlavorCPUSpecType = TypedDict(
         'stepping': Optional[int],
         'flag': Optional[list[str]],
         'processors': Optional[int],
+        'threads-per-core': Optional[int],
     },
 )
 
@@ -838,6 +839,9 @@ def _apply_flavor_specification(flavor: Flavor, flavor_spec: ConfigFlavorSpecTyp
 
         if 'processors' in cpu_patch:
             flavor.cpu.processors = cpu_patch['processors']
+
+        if 'threads-per-core' in cpu_patch:
+            flavor.cpu.threads_per_core = cpu_patch['threads-per-core']
 
         if 'family' in cpu_patch:
             flavor.cpu.family = cpu_patch['family']
